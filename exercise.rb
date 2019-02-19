bca09276a093309da8900fbd567d09a35e45602c
Singalt16 <singalt16@gmail.com>
2019-02-18 20:47:59 -0500

    added file with functions for testing

---
+++ exercise.rb
@@ -0,0 +1,19 @@
+# adds two numbers
+def add(x, y)
+
+  if not x.is_a? Numeric or not y.is_a? Numeric
+    raise ArgumentError.new("arguments must be numbers")
+  end
+
+  x * y # incorrect
+end
+
+# subtracts two numbers
+def subtract(x, y)
+
+  if not x.is_a? Numeric or not y.is_a? Numeric
+    raise ArgumentError.new("arguments must be numbers")
+  end
+
+  x - y
+end
